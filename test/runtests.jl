21dfe8e55d403cbbab3afc650c19f13328721bc8
R. C. Thomas <rcthomas@lbl.gov>
2015-11-13 11:27:36 -0800

    Trying to develop tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Base.Test
 
 #@test_approx_eq planck( [ 5000.0 ], 5000.0, PerHertz() ) [ 1.00964e-5 ] make better test
 
-@test 1 == 1
+f( wl ) = planck( wl, 12000.0, PerAngstrom )
+
+@test_approx_eq quadgk( f, 0.0, 1.0e9 )[ 1 ] stefan_boltzmann( 12000.0 )
 
 

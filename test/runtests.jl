8e4f5fdebe2cda789febb936cc6ef0c550502624
R. C. Thomas <rcthomas@lbl.gov>
2015-11-13 11:46:01 -0800

    More chatty.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,13 @@ using Base.Test
 
 using Blackbody
 
-for i = 1:20
-    temperature = 1000.0 * i
-    @test_approx_eq_eps quadgk( wl -> planck( wl, temperature, PerAngstrom ), 0.0, 1.0e9 )[ 1 ] / stefan_boltzmann( temperature ) 1.0 1.0e-7
+custom_handler( r::Test.Success ) = println( "PASS $(r.expr)" )
+custom_handler( r::Test.Failure ) = println( "FAIL $(r.expr)" )
+custom_handler( r::Test.Error   ) = rethrow( r )
+
+Test.with_handler( custom_handler ) do
+    for i = 1:20
+        temperature = 1000.0 * i
+        @test_approx_eq_eps quadgk( wl -> planck( wl, temperature, PerAngstrom ), 0.0, 1.0e9 )[ 1 ] / stefan_boltzmann( temperature ) 1.0 1.0e-7
+    end
 end

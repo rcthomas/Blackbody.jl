ea045c9522b03afcec8986566c8009dc7b701d0b
R. C. Thomas <rcthomas@lbl.gov>
2015-11-13 11:41:17 -0800

    Trying more of these just for fun --- the test framework isn't very chatty and doesn't make me feel as happy as with gtest.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,8 @@
-
-using Blackbody
 using Base.Test
 
-#@test_approx_eq planck( [ 5000.0 ], 5000.0, PerHertz() ) [ 1.00964e-5 ] make better test
+using Blackbody
 
-@test_approx_eq_eps quadgk( wl -> planck( wl, 12000.0, PerAngstrom ), 0.0, 1.0e9 )[ 1 ] / stefan_boltzmann( 12000.0 ) 1.0 1.0e-7
-
-
+for i = 1:20
+    temperature = 1000.0 * i
+    @test_approx_eq_eps quadgk( wl -> planck( wl, temperature, PerAngstrom ), 0.0, 1.0e9 )[ 1 ] / stefan_boltzmann( temperature ) 1.0 1.0e-7
+end

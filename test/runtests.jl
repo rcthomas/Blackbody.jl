5f4c3e202df23c998845a248d9aa36674844090e
R. C. Thomas <rcthomas@lbl.gov>
2015-11-13 11:39:21 -0800

    Some learning of anonymous functions maybe?

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,6 @@ using Base.Test
 
 #@test_approx_eq planck( [ 5000.0 ], 5000.0, PerHertz() ) [ 1.00964e-5 ] make better test
 
-f( wl ) = planck( wl, 12000.0, PerAngstrom )
-
-@test_approx_eq_eps quadgk( f, 0.0, 1.0e9 )[ 1 ] / stefan_boltzmann( 12000.0 ) 1.0 1.0e-14
+@test_approx_eq_eps quadgk( wl -> planck( wl, 12000.0, PerAngstrom ), 0.0, 1.0e9 )[ 1 ] / stefan_boltzmann( 12000.0 ) 1.0 1.0e-7
 
 
